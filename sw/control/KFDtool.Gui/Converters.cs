5b4b3838db3e5eec1baf040b613e77217def062d
Daniel Dugger <daniel@danieldugger.com>
2019-08-25 17:33:45 -0400

    Show name of algorithm in view key info

---
+++ sw/control/KFDtool.Gui/Converters.cs
@@ -0,0 +1,33 @@
+﻿using KFDtool.P25.Constant;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace KFDtool.Gui.Converters
+{
+    public class AlgorithmFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int intValue = (int)value;
+
+            if (Enum.IsDefined(typeof(AlgorithmId), (byte)intValue))
+            {
+                return string.Format("{0} (0x{0:X}) {1}", intValue, ((AlgorithmId)intValue).ToString());
+            }
+            else
+            {
+                return string.Format("{0} (0x{0:X} UNKNOWN)", intValue);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
